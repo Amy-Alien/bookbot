a35150fa7a099e3b49f9f3904ca34475033652ba
Amy-Alien <hughsence@gmail.com>
2024-11-19 21:41:37 +0400

    add generate_report func

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import string
 
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
-    print("The number of words in the book:", count_words(file_contents))
-    print(count_characters(file_contents))
 
 def count_words(text):
     words = text.split()
@@ -14,11 +8,42 @@ def count_words(text):
 def count_characters(text):
     text = text.lower()
     char_count = {}
+
     for char in text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-                
+        if char.isalpha():
+            if char in char_count: # Если буква в словаре есть мы прибавлем 1 к счетчику
+                char_count[char] += 1
+            else: # Если буквы в словаре нет мы инициализируем счетчик для этого символа
+                char_count[char] = 1
+    
+    return char_count
+            
+def generate_report(file_path):
+    
+    with open(file_path) as file:
+        text = file.read()
+        
+    word_count = count_words(text)
+    char_count = count_characters(text)
+    
+    # Print report
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{word_count} words found in the document")
+
+    # Sort chars
+    sorted_char = sorted(char_count.items(), key= lambda item: item[1], reverse=True)
+    # Print sorted chars
+    for char, count in sorted_char:
+        print(f"The '{char}' character was found {count} items")
+    
+    print("--- End report ---")
+        
+
+def main():
+    file_path = "books/frankenstein.txt"
+
+    generate_report(file_path)
+    
+
 if __name__ == "__main__":
     main()
